c676961afbcfc0cefe028a4b245d17461b3e2c38
limedosa <109569567+limedosa@users.noreply.github.com>
2023-03-20 20:25:40 -0400

    pt 1

---
+++ homepage/index.js
@@ -0,0 +1,29 @@
+NONE OF THIS IS USED SO DISREGARD!!!!!!!!!!!!!!!
+
+// function toggle() {
+//     var div = document.getElementById("myDiv");
+//     if (div.style.display === "none") {
+//       div.style.display = "block";
+//     } else {
+//       div.style.display = "none";
+//     }
+//   }
+//   // create the div element
+// var googleTranslateElement = document.createElement('div');
+// googleTranslateElement.id = 'google_translate_element';
+
+// // append the div to the body
+// document.body.appendChild(googleTranslateElement);
+
+// // create the script element
+// var googleTranslateScript = document.createElement('script');
+// googleTranslateScript.type = 'text/javascript';
+// googleTranslateScript.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+
+// // append the script to the body
+// document.body.appendChild(googleTranslateScript);
+
+// // define the googleTranslateElementInit function
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+// }
